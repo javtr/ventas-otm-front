22af6231420b64c93e3bc733230a4cb89cd53758
javtr <javtriana.z@gmail.com>
2022-09-27 22:40:31 -0500

    init

--- src/components/pure/forms/formVenta.jsx
+++ src/components/pure/forms/formVenta.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { Form, Formik, Field } from "formik";
 
 export default function FormVenta() {
-  return(
+  return (
     <div className="layout__container--form">
-      FormVenta
+      <Formik 
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form className="formik">
+          <label className="formik--label"
+          htmlFor="firstName">First Name</label>
+          <Field className="formik--field"
+          id="firstName" name="firstName" placeholder="Jane" />
+
+          <label className="formik--label"
+          htmlFor="lastName">Last Name</label>
+          <Field className="formik--field"
+          id="lastName" name="lastName" placeholder="Doe" />
+
+          <label className="formik--label"
+          htmlFor="email">Email</label>
+          <Field className="formik--field"
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <button className="formik--button"
+          type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
