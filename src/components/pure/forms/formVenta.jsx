4c53db002457e9fe092142658fa0cb0df98bd975
javtr <javtriana.z@gmail.com>
2022-09-28 22:52:37 -0500

    formik

--- src/components/pure/forms/formVenta.jsx
+++ src/components/pure/forms/formVenta.jsx
@@ -1,72 +1,146 @@
-import React from "react";
-import { Form, Formik, Field } from "formik";
+import React,{useState} from 'react';
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function FormVenta() {
 
+  const [filas, setFilas] = useState(1);
 
+
+//platilla de errores
+  const loginSchema = Yup.object().shape({
+    firstName: Yup.string().required("nombre requerido"),
+    lastName: Yup.string().required("nombre requerido"),
+    email: Yup.string()
+      .email("formato de correo invalido")
+      .required("correo requerido"),
+  });
+
+  //manejo de envio de formulario
   function submitHandle(values) {
-    
-
-    const valuesSend={
-      "nombre": values.firstName + " " +values.lastName,
-      "correo": values.email
-    }
+    const valuesSend = {
+      nombre: values.firstName,
+      apellido: values.lastName,
+      correo: values.email,
+    };
 
     console.log(valuesSend);
 
-
-    fetch("http://localhost:8080/cliente/save",{
-      method:"POST",
-      headers:{"Content-Type":"application/json"},
-      body:JSON.stringify(valuesSend)
-
-  }).then(()=>{
-    console.log("New Student added")
-  })
-
-
-
-
+    fetch("http://localhost:8080/cliente/save", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(valuesSend),
+    }).then(() => {
+      console.log("cliente agregado");
+    });
   }
+
+  //crear campos con boton
+  function crearFields() {
+    let fieldsCont = [];
+
+    for (let i = 1; i <= filas; i++) {
+
+      const idIt1 = `firstName${i}`;
+
+      fieldsCont.push(
+        <div key={i}>
+          <label className="formik--label" htmlFor={idIt1}>
+            First Name2
+          </label>
+          <Field
+            className="formik--field"
+            id= {idIt1}
+            name={idIt1}
+            placeholder="Jane2"
+          />
+        </div>
+      );
+    }
+
+    return fieldsCont;
+  }
+
+  function aumentaFilas() {
+    setFilas(filas+1)
+}
+
+
 
   return (
     <div className="layout__container--form">
-      <Formik 
+      <Formik
         initialValues={{
           firstName: "",
           lastName: "",
           email: "",
         }}
+        validationSchema={loginSchema}
         onSubmit={async (values) => {
           submitHandle(values);
-
-
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
         }}
       >
-        <Form className="formik">
-          <label className="formik--label"
-          htmlFor="firstName">First Name</label>
-          <Field className="formik--field"
-          id="firstName" name="firstName" placeholder="Jane" />
+        {({
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+        }) => (
+          <Form className="formik">
+            <label className="formik--label" htmlFor="firstName">
+              First Name
+            </label>
+            <Field
+              className="formik--field"
+              id="firstName"
+              name="firstName"
+              placeholder="Jane"
+            />
 
-          <label className="formik--label"
-          htmlFor="lastName">Last Name</label>
-          <Field className="formik--field"
-          id="lastName" name="lastName" placeholder="Doe" />
+            {errors.firstName && touched.firstName && (
+              <ErrorMessage name="firstName" component="div"></ErrorMessage>
+            )}
 
-          <label className="formik--label"
-          htmlFor="email">Email</label>
-          <Field className="formik--field"
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-          />
-          <button className="formik--button"
-          type="submit">Submit</button>
-        </Form>
+            <label className="formik--label" htmlFor="lastName">
+              Last Name
+            </label>
+            <Field
+              className="formik--field"
+              id="lastName"
+              name="lastName"
+              placeholder="Doe"
+            />
+
+            {errors.lastName && touched.lastName && (
+              <ErrorMessage name="lastName" component="div"></ErrorMessage>
+            )}
+
+            <label className="formik--label" htmlFor="email">
+              Email
+            </label>
+            <Field
+              className="formik--field"
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+            />
+
+            {errors.email && touched.email && (
+              <ErrorMessage name="email" component="div"></ErrorMessage>
+            )}
+
+            {crearFields()}
+            <button type='button' onClick={aumentaFilas}>aumentar</button>
+
+
+            <button className="formik--button" type="submit">
+              Submit
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
