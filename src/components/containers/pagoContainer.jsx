1bff066d3d9ce86447c255a7dd0190d1158425c5
javtr <javtriana.z@gmail.com>
2022-10-17 17:51:48 -0500

    registro de pagos

--- src/components/containers/pagoContainer.jsx
+++ src/components/containers/pagoContainer.jsx
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from "react";
 import PagoRow from "../pure/PagoRow";
-import { GetPagos, GetQueryPagosFecha } from "../../Services/axiosService";
+import {
+  GetPagos,
+  GetQueryPagosFecha,
+  GetAllMedios,
+} from "../../Services/axiosService";
 import { useForm, useFieldArray, Controller } from "react-hook-form";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const PagoContainer = () => {
+  //declaraciones ------------------------------------------------
   const { register, control, handleSubmit, watch } = useForm();
   const [pagos, setPagos] = useState([]);
   const [filtro, setFiltro] = useState("");
   const [filtroDateIni, setFiltroDateIni] = useState("");
   const [filtroDateFin, setFiltroDateFin] = useState("");
+  const [medios, setMedios] = useState([]);
+  const pagoSecciones = [];
+  const TotalSecciones = [];
+  const GranTotal = {
+    bruto:0,
+    neto:0
+  };
 
+  //llamados ------------------------------------------------
   useEffect(() => {
     obtainPagos();
+    obtainMedios();
   }, []);
 
   function onSubmitForm(data) {
@@ -37,6 +51,7 @@ const PagoContainer = () => {
     return [year, month, day].join("-");
   }
 
+  //axios ------------------------------------------------
   const obtainPagos = () => {
     GetPagos()
       .then((response) => {
@@ -52,6 +67,17 @@ const PagoContainer = () => {
     GetQueryPagosFecha(dataForm)
       .then((response) => {
         setPagos(response.data);
+      })
+      .catch((error) => {
+        alert(`Somethin went wrong: ${error}`);
+      })
+      .finally(() => {});
+  };
+
+  const obtainMedios = () => {
+    GetAllMedios()
+      .then((response) => {
+        setMedios(response.data);
       })
       .catch((error) => {
         alert(`Somethin went wrong: ${error}`);
@@ -101,15 +127,58 @@ const PagoContainer = () => {
     });
   }
 
-
+  //ordenar los datos
   resultados = resultados.sort((p1, p2) =>
     p1.fechaPago > p2.fechaPago ? 1 : p1.fechaPago < p2.fechaPago ? -1 : 0
   );
 
+  //dividir resultados en medios de pago ---------------------------------------------------------
+
+  if (medios.length > 0 && resultados.length > 0) {
+    for (let i = 0; i < medios.length; i++) {
+      const medio = medios[i].medioPago;
+
+      if (resultados.length > 0) {
+        pagoSecciones.push(
+          resultados.filter(function (pago) {
+            if (pago.facturaPago.medioPagoFactura.medioPago === medio) {
+              return true;
+            } else {
+              return false;
+            }
+          })
+        );
+      }
+    }
+  }
+
+  //calcular totales ----------------------------------------------
+
+  if (pagoSecciones.length > 0) {
+    for (let i = 0; i < pagoSecciones.length; i++) {
+      const seccion = pagoSecciones[i];
+
+      TotalSecciones.push({
+        valorBruto: seccion.reduce(function (acc, obj) {
+          return Math.round(acc + obj.valorPago);
+        }, 0),
+
+        valorNeto: seccion.reduce(function (acc, obj) {
+          return Math.round(acc + obj.valorPagoNeto);
+        }, 0),
+
+      });
+    }
+
+  }
 
 
-  
-
+  if (TotalSecciones.length>0) {
+  TotalSecciones.map((seccion)=>{
+    GranTotal.bruto = GranTotal.bruto + seccion.valorBruto;
+    GranTotal.neto = GranTotal.neto + seccion.valorNeto;
+  })
+}
 
 
   return (
@@ -157,25 +226,48 @@ const PagoContainer = () => {
 
           {/* <button type="submit">Submit</button> */}
         </form>
+
+        {/* Tabla ------------------------------------- */}
       </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Fecha</th>
-            <th>Desembolso</th>
-            <th>Valor</th>
-            <th>Neto</th>
-            <th>Medio</th>
-            <th>Cliente</th>
-          </tr>
-        </thead>
-        <tbody>
-          {resultados.map((pago, index) => (
+      {pagoSecciones
+        ? pagoSecciones.map((seccion, index) => (
+            <div key={index}>
+              {seccion.length > 0 && TotalSecciones.length > 0 ? (
+                <div>
+                  <h3>{seccion[0].facturaPago.medioPagoFactura.medioPago}</h3>
+                  <h5>Bruto: {TotalSecciones[index].valorBruto}</h5>
+                  <h5>Neto: {TotalSecciones[index].valorNeto}</h5>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Fecha</th>
+                        <th>Desembolso</th>
+                        <th>Valor</th>
+                        <th>Neto</th>
+                        <th>Medio</th>
+                        <th>Cliente</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {seccion.map((pago, index2) => (
+                        <PagoRow key={index2} pago={pago}></PagoRow>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          ))
+        : resultados.map((pago, index) => (
             <PagoRow key={index} pago={pago}></PagoRow>
           ))}
-        </tbody>
-      </table>
+
+    <h2>Totales</h2>    
+    <h4>Bruto: {GranTotal.bruto}</h4>
+    <h4>Neto: {GranTotal.neto}</h4>
     </div>
   );
 };
--- src/components/pure/PagoRow.jsx
+++ src/components/pure/PagoRow.jsx
@@ -5,8 +5,8 @@ const PagoRow = ({pago}) => {
         <tr>
         <td>{pago.fechaPago}</td>
         <td>{pago.fechaDesembolso}</td>
-        <td>{pago.valorPago}</td>
-        <td>{pago.valorPagoNeto}</td>
+        <td>{Math.round(pago.valorPago)}</td>
+        <td>{Math.round(pago.valorPagoNeto)}</td>
         <td>{pago.facturaPago.medioPagoFactura.medioPago}</td>
         <td>{pago.facturaPago.clienteFactura.nombre + " " + pago.facturaPago.clienteFactura.apellido}</td>
       </tr>
