990e034ffe635bd33abc87e41ed1fa4a1bcc209a
javtr <javtriana.z@gmail.com>
2022-10-29 22:59:54 -0500

    auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import "./App.css";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
@@ -17,34 +17,52 @@ import FormRegister from "./components/pure/forms/formRegister";
 import FormLogin from "./components/pure/forms/formLogin";
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+export default function App() {
 
+  // useEffect(() => {
+  //   localStorage.token="",
+  //   localStorage.email=""
+  // }, []);
 
-        <Routes>
-          <Route path="/" element={<LayoutVenta></LayoutVenta>}>
-            <Route index element={<Home></Home>}></Route>
-            <Route path="sale" element={<FormComp></FormComp>}></Route>
-            <Route path="reg-clientes" element={<RegClientesCont></RegClientesCont>}></Route>
-            <Route path="pagos" element={<PagoContainer></PagoContainer>}></Route>
-            <Route path="cliente/:userId" element={<UserContainer></UserContainer>}></Route>
-            <Route path="edit-cliente/:userId" element={<FormEditCliente></FormEditCliente>}></Route>
-            <Route path="edit-factura/:facturaId" element={<FormEditFactura></FormEditFactura>}></Route>
-            <Route path="edit-pago/:pagoId" element={<FormEditPago></FormEditPago>}></Route>
-            <Route path="edit-compra/:compraId" element={<FormEditCompra></FormEditCompra>}></Route>
-            <Route path="add-pago/:pagoId" element={<FormAddPago></FormAddPago>}></Route>
-          </Route>
-          <Route path="/register" element={<FormRegister></FormRegister>}></Route>
-          <Route path="/login" element={<FormLogin></FormLogin>}></Route>
-          
-        </Routes>
-
-
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LayoutVenta></LayoutVenta>}>
+          <Route index element={<Home></Home>}></Route>
+          <Route path="sale" element={<FormComp></FormComp>}></Route>
+          <Route
+            path="reg-clientes"
+            element={<RegClientesCont></RegClientesCont>}
+          ></Route>
+          <Route path="pagos" element={<PagoContainer></PagoContainer>}></Route>
+          <Route
+            path="cliente/:userId"
+            element={<UserContainer></UserContainer>}
+          ></Route>
+          <Route
+            path="edit-cliente/:userId"
+            element={<FormEditCliente></FormEditCliente>}
+          ></Route>
+          <Route
+            path="edit-factura/:facturaId"
+            element={<FormEditFactura></FormEditFactura>}
+          ></Route>
+          <Route
+            path="edit-pago/:pagoId"
+            element={<FormEditPago></FormEditPago>}
+          ></Route>
+          <Route
+            path="edit-compra/:compraId"
+            element={<FormEditCompra></FormEditCompra>}
+          ></Route>
+          <Route
+            path="add-pago/:pagoId"
+            element={<FormAddPago></FormAddPago>}
+          ></Route>
+        </Route>
+        <Route path="/register" element={<FormRegister></FormRegister>}></Route>
+        <Route path="/login" element={<FormLogin></FormLogin>}></Route>
+      </Routes>
+    </div>
+  );
 }
-
-export default App;
