92635388242a4375f07b7f0c235a2809fa6de2fe
javtr <javtriana.z@gmail.com>
2023-01-09 19:02:28 -0500

    cambio de server

--- src/Services/config/axiosConfig.js
+++ src/Services/config/axiosConfig.js
@@ -7,7 +7,12 @@ export default axios.create({
 
   // baseURL: localStorage.conection == 0? "https://ventasotmv2-production.up.railway.app":"http://localhost:8080",
   // baseURL: "http://localhost:8080",
-  baseURL: "https://ventasotmv2-production.up.railway.app/",
+  // baseURL: "https://ventasotmv2-production.up.railway.app/",
+  baseURL: "https://ventas2.fly.dev/",
+  
+
+  // https://ventas2.fly.dev/
+
 
   responseType: "json",
   // timeout: 6000
