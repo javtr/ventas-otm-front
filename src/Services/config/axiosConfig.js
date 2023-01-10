35bf7d6a9297a4d43de885a4939c679d63faa74d
javtr <javtriana.z@gmail.com>
2023-01-10 10:23:23 -0500

    server

--- src/Services/config/axiosConfig.js
+++ src/Services/config/axiosConfig.js
@@ -8,10 +8,13 @@ export default axios.create({
   // baseURL: localStorage.conection == 0? "https://ventasotmv2-production.up.railway.app":"http://localhost:8080",
   // baseURL: "http://localhost:8080",
   // baseURL: "https://ventasotmv2-production.up.railway.app/",
-  baseURL: "https://white-forest-8775.fly.dev/",
+  baseURL: "https://blue-moon-9351.fly.dev/",
   
 
+  // https://white-forest-8775.fly.dev/
   // https://ventas2.fly.dev/
+
+  
 
 
   responseType: "json",
